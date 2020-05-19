8d1f88b940fb3fbd1266e9a2fc9ed15fa6cbbcb4
Ujjwal Chakraborty <ujjwal.chakraborty.01@gmail.com>
2020-05-19 14:26:35 +0530

    Create Day_19.cpp

---
+++ Day_19.cpp
@@ -0,0 +1,26 @@
+// Time Complexity : O(N)
+// Space Complexity : O(N)
+
+class StockSpanner {
+public:
+    stack<pair<int , int> > s;
+    StockSpanner() {
+        
+    }
+    
+    int next(int price) {
+        int val=1;
+        while(!s.empty() && price >= s.top().first){
+            val+=s.top().second;
+            s.pop();
+            }         
+	      s.push({price , val});
+        return val;
+    }
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
