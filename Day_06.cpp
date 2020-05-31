95169f3b52cb862ed373226bea6896ee665ce935
Ujjwal Chakraborty <ujjwal.chakraborty.01@gmail.com>
2020-05-31 15:52:44 +0530

    Create Day_06.cpp

---
+++ Day_06.cpp
@@ -0,0 +1,14 @@
+// Time Complexity : O(N)
+// Space Complexity : O(N)
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int , int> m;
+        for(auto i : nums){
+            m[i]++;
+            if(m[i] > (nums.size()/2))
+                return i;
+        }
+        return 0;
+    }
+};
