8ed9d8286ebcc0007ff1d0ae2f6fe4e4b949125d
Ujjwal Chakraborty <ujjwal.chakraborty.01@gmail.com>
2020-05-31 15:45:52 +0530

    Create Day_05.cpp

---
+++ Day_05.cpp
@@ -0,0 +1,14 @@
+//Time Complexity : O(N)
+//Space Complexity : O(N)
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        unordered_map<char , int > m;
+        for(auto i : s)
+            m[i]++;
+        for(int i=0;i<s.length();i++)
+            if(m[s[i]]==1)
+                return i;
+        return -1;
+    }
+};
