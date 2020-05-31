989203360f7d85385790fc52bdf54b80441b9730
Ujjwal Chakraborty <ujjwal.chakraborty.01@gmail.com>
2020-05-31 18:59:44 +0530

    Create Day_08.cpp

---
+++ Day_08.cpp
@@ -0,0 +1,16 @@
+//Time Complexity : O(N)
+//Space Complexity : O(N)
+class Solution {
+public:
+    bool checkStraightLine(vector<vector<int>>& coordinates) {
+        int x0 = coordinates[0][0], y0 = coordinates[0][1], 
+            x1 = coordinates[1][0], y1 = coordinates[1][1];
+        int dx = x1 - x0, dy = y1 - y0;
+        for (auto i : coordinates) {
+            int x = i[0], y = i[1];
+            if (dx * (y - y1) != dy * (x - x1))
+                return false;
+        }
+        return true;
+    }
+};
