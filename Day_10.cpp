e5eac20221f1c9445a9eacc264ede31f9ad5935a
Ujjwal Chakraborty <ujjwal.chakraborty.01@gmail.com>
2020-06-01 19:14:54 +0530

    Create Day_10.cpp

---
+++ Day_10.cpp
@@ -0,0 +1,16 @@
+// Time Complexity : O(N)
+// Space Complexity : O(N)
+class Solution {
+public:
+    int findJudge(int N, vector<vector<int>>& trust) {
+        vector<int> count(N+1 , 0);
+        for(auto i : trust){
+            count[i[0]]--;
+            count[i[1]]++;
+        }
+        for(int i=1;i<count.size();i++)
+            if(count[i]==N-1)
+                return i;
+        return -1;
+    }
+};
